f302b86dfd65d7785ac962145b3cdf9cd7b30fdf
ebarroso2214 <ebarroso2214@gmail.com>
2023-08-09 20:12:47 -0400

    test commit

--- backend/models/index.js
+++ backend/models/index.js
@@ -8,7 +8,7 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(()=> console.log('MongoDB connected'))
+.then(()=> console.log('MongoDB has connected'))
 .catch(e=>console.log(e));
 
 module.exports.User = require('./user.js');
