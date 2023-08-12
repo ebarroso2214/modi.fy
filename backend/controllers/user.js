da8d19a41f3d63161cebe0926cce4a5889a897ae
ebarroso2214 <ebarroso2214@gmail.com>
2023-08-12 10:24:16 -0400

    set up all routes for users

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -24,6 +24,39 @@ router.get('/:id', async(req, res) => {
     }
 })
 
+//Create user
+router.post('/', async(req, res) => {
+    try{
+        const createdUser = await User.create(req.body)
+        res.status(200).json(createdUser)
+    }
+    catch(err){
+        res.status(400).json({error: err})
+    }
+})
+
+//Update user
+router.put('/:id', async(req,res) => {
+    try{
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({message: 'Updated User'})
+    }
+    catch(err){
+        res.status(400).json({error: err})
+    }
+})
+
+//Delete user
+router.delete('/:id', async(req,res) => {
+    try{
+        const deletedUser = await User.findByIdAndDelete(req.params.id)
+        res.status(200).json({message: "Deleted User"})
+    }
+    catch(err){
+        res.status(400).json({error: err})
+    }
+})
+
 
 
 router.get('*', (req,res) => {
